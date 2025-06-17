0a5f53e8bdc7457da2d74f23a26df1b1e9188f1f
GMA <98269239+gmanand23@users.noreply.github.com>
2025-06-17 10:18:46 +0530

    app.js

--- app.js
+++ app.js
@@ -153,7 +153,7 @@ function startScanner() {
 
   qrReader.start(
     { facingMode: "environment" },
-    { fps: 10, qrbox: 800 },
+    { fps: 10, qrbox: 600 },
     (decodedText) => {
       document.getElementById('coilInput').value = decodedText;
       searchCoil();
